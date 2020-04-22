2739209eafc8d664b378a149931da64e4852066b
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2020-04-22 11:35:48 -0300

    Configured the IdentityServer to Log In with the Authorization Code Flow

--- src/Marvin.IDP/Config.cs
+++ src/Marvin.IDP/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -22,7 +23,23 @@ namespace Marvin.IDP
         
         public static IEnumerable<Client> Clients =>
             new Client[] 
-            { };
-        
+            { new Client {
+                ClientName = "Image Gallery",  //This will appear in the consent screen and log in
+                ClientId = "imagegalleryclient",
+                AllowedGrantTypes = GrantTypes.Code,  //Working with the Code Flow
+                RedirectUris = new List<string>()
+                {
+                    "https://localhost:44389/signin-oidc"  //The code is delivered to a browser by URI redirection
+                },
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                },
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256()) //To allow the client application to call the token endpoint
+                }
+            } };        
     }
 }
